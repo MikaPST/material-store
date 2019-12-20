c9d2d30bccbe1cf30865a3b1d8b29686bb1cc7fa
MikaPST <michael.pastor@outlook.fr>
2019-12-20 12:18:28 +0100

    update bootstrap

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,20 +17,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
+     * @param CategoryRepository $categoryRepository
+     * @param CacheItemPoolInterface $pool
      * @Route("/", name="category_index", methods={"GET"})
+     * @return Response
+     * @throws \Psr\Cache\InvalidArgumentException
      */
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, CacheItemPoolInterface $pool): Response
     {
+        $key = "categories";
+        $item = $pool->getItem($key);
+        if (!$item->isHit()) {
+            $item->set($categoryRepository->findAll());
+            $pool->save($item);
+        }
+
         return $this->render('category/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
         ]);
     }
 
     /**
+     * @param Request $request
+     * @param CacheItemPoolInterface $cache
+     * @param CategoryRepository $repository
+     * @return Response
      * @Route("/new", name="category_new", methods={"GET","POST"})
+     * @throws \Psr\Cache\InvalidArgumentException
      */
-    public function new(Request $request): Response
+    public function new(Request $request, CacheItemPoolInterface $pool, CategoryRepository $repository): Response
     {
+
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
@@ -38,6 +56,17 @@ class CategoryController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($category);
             $entityManager->flush();
+
+            $key = "categories";
+            $item = $pool->getItem($key);
+            $categories = $item->get();
+            if (!$categories) {
+                $categories = $repository->findAll();
+            } else {
+                $categories[] = $form->getData();
+            }
+            $item->set($categories);
+            $pool->save($item);
 
             return $this->redirectToRoute('category_index');
         }
@@ -83,7 +112,7 @@ class CategoryController extends AbstractController
      */
     public function delete(Request $request, Category $category): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($category);
             $entityManager->flush();
